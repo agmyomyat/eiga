82b40510dfc1dc8da5e7219d0731bb3c7c0920c4
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-07-01 15:21:24 +0630

    added: interval to check transaction_status

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { useTransaction_StatusLazyQuery } from '@graphgen'
 export default function Checkout() {
    const router = useRouter()
    const { transactionId, orderId, PWAToken, qrCode } = router.query
+   const [getTransactionStatus] = useTransaction_StatusLazyQuery({
+      fetchPolicy: 'no-cache',
+   })
    useEffect(() => {
       console.count('reRender count')
       console.log('trasactionid', transactionId)
@@ -10,5 +14,30 @@ export default function Checkout() {
       console.log('qrcode', qrCode)
       console.log('pwaToken', PWAToken)
    }, [PWAToken, orderId, qrCode, transactionId])
+   useEffect(() => {
+      let getTransaction: NodeJS.Timer
+      if (transactionId && orderId) {
+         getTransaction = setInterval(() => {
+            // eslint-disable-next-line @typescript-eslint/no-extra-semi
+            void (async () => {
+               const _transaction_status = await getTransactionStatus({
+                  variables: {
+                     transactionId: transactionId as string,
+                     orderId: orderId as string,
+                  },
+               })
+               console.log(
+                  'transaction_status',
+                  _transaction_status.data.transaction_status.transactionStatus
+               )
+            })()
+         }, 3000) //run
+         return
+      }
+      void router.replace('/404')
+      return () => {
+         if (getTransaction) clearInterval(getTransaction)
+      }
+   }, [getTransactionStatus, orderId, router, transactionId])
    return <div>Checkout</div>
 }

fa77713c97dee98513ac499b9100664ab0d66263
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-07-01 14:48:45 +0630

    added: console log for query params

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 export default function Checkout() {
+   const router = useRouter()
+   const { transactionId, orderId, PWAToken, qrCode } = router.query
+   useEffect(() => {
+      console.count('reRender count')
+      console.log('trasactionid', transactionId)
+      console.log('orderid', orderId)
+      console.log('qrcode', qrCode)
+      console.log('pwaToken', PWAToken)
+   }, [PWAToken, orderId, qrCode, transactionId])
    return <div>Checkout</div>
 }

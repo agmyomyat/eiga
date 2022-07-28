be771c368a8e225ef119c618808d7852ad0664a2
urnotalone2 <54217097+urnotalone2@users.noreply.github.com>
2022-07-28 09:53:47 +0630

    removed publishat to return null in movie component

--- components/movies/Movies.tsx
+++ components/movies/Movies.tsx
@@ -9,7 +9,6 @@ const Movies: React.FC<IMovies> = ({ movies }) => {
    return (
       <Box display="grid" rowGap={3} columnGap={2} sx={grid}>
          {movies?.map((movie: TMovieProps) => {
-            if (!movie.published_at) return null
             return <Movie key={movie.id} {...movie} />
          })}
       </Box>
